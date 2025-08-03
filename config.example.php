dba30d5ce01256afb8dc85dd0b9e8fb5134d0027
misakanetwork2018 <misakanetwork2018@gmail.com>
2025-08-03 19:37:18 +0800

    feat: 添加默认助手函数route

--- config.example.php
+++ config.example.php
@@ -3,6 +3,7 @@
 return [
     'maintain' => false,
     'debug' => false,
+    'rewrite' => false,
     'db' => [
         'host' => '127.0.0.1',
         'port' => 3306,
--- modules/functions.php
+++ modules/functions.php
@@ -1,3 +1,39 @@
 <?php
 
 // 这里可以放置全局助手函数
+
+use EasyFrameworkCore\App;
+
+if (!function_exists('route')) {
+    /**
+     * 获取路由链接
+     *
+     * @param $module
+     * @param $action
+     * @param array $queries
+     * @return string
+     * @throws \EasyFrameworkCore\Exception\ClassNotExistException
+     */
+    function route($module, $action, array $queries = []): string
+    {
+        $useQueries = false;
+
+        if (App::config('rewrite')) {
+            $url = "/$module/$action";
+        } else {
+            $url = "?m=$module&a=$action";
+            $useQueries = true;
+        }
+
+        foreach ($queries as $k => $v) {
+            if ($useQueries) {
+                $url .= "&$k=$v";
+            } else {
+                $url .= "?$k=$v";
+                $useQueries = true;
+            }
+        }
+
+        return $url;
+    }
+}
